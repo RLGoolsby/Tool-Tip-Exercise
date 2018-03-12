c668c5bf5418d7c02ca5cb0f5ea459835be4b56e
Rhonda Goolsby <rlgoolsby@gmail.com>
2018-03-12 16:03:18 -0400

    Add styling to tooltip

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 // When the user clicks on div, open the popup
 function myFunction() {
-    var popup = document.getElementById("myPopup");
+    var popup = document.getElementById("myPopup-right");
     popup.classList.toggle("show");
 }
